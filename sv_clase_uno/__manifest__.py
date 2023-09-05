ffff3752097cc4f4e521b1862af47d52191fb91f
Enmanuel Ortiz <enmanuele777@gmail.com>
2023-09-04 19:49:30 -0600

    Academia example

---
+++ sv_clase_uno/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+	"name": "Academia",
+	"version": "1.0.0",
+	"author": "OdooES El Salvador",
+	"category": "Others",
+	"summary": "Academia",
+	"license": "LGPL-3",
+	"support": "enmanuele777@gmail.com",
+	"contributors": [
+		'Enmanuel Ortiz <enmanuele777@gmail.com>',
+	],
+	"maintainer": "Enmanuel Ortiz",
+	"website": "https://odooes.com",
+	"description": """Academia""",
+	
+	"depends": [
+		"base",
+	],
+	"data": [
+	],
+	"demo": [
+	],
+	"images": [
+	],
+	"installable": True,
+	"application": False,
+	"auto_install": False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

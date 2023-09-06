06d56412e3801fc4567d271745985fa27580a8df
Enmanuel Ortiz <enmanuele777@gmail.com>
2023-09-05 20:34:58 -0600

    [ADD] Clase 3 agregando modelos

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+	"name": "Motorcycle Registry",
+	"version": "0.0.1",
+	"author": "OdooES El Salvador",
+	"category": "Kawiil",
+	"summary": "Manage Registration of Motorcycle",
+	"license": "OPL-1",
+	"support": "enmanuele777@gmail.com",
+	"maintainer": "Enmanuel Ortiz",
+	"website": "https://odooes.com",
+	"description": """Motorcycle Registry
+====================
+This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+""",
+	
+	"depends": [
+		"base",
+	],
+	"data": [
+	],
+	"demo": [
+	],
+	"images": [
+	],
+	"installable": True,
+	"application": True,
+	"auto_install": False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

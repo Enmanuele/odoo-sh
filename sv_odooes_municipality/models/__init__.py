dff63c895fe8a29ec6a6d55f581e322ce7c97b9c
Enmanuel Ortiz <enmanuele777@gmail.com>
2023-09-05 20:56:48 -0600

    removiendo modulos

--- sv_odooes_municipality/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import municipality
-from . import res_partner
-from . import res_company
--- sv_odooes_municipality/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-class ResPartner(models.Model):
-	_inherit = 'res.partner'
-	_description = 'Contacto'
-	
-	municipality_id = fields.Many2one('res.municipality', 'Municipio', required=True)

0f6134d14a59bd21248c5ca215f6a459be534351
Enmanuel Ortiz <enmanuele777@gmail.com>
2023-09-13 18:12:29 -0600

    Actualizacion, sitio web y mapas

---
+++ motorcycle_map/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+	"name": "Motorcycle Map",
+	"version": "0.0.1",
+	"author": "OdooES El Salvador",
+	"category": "Kawiil",
+	"summary": "Manage Map View",
+	"license": "OPL-1",
+	"support": "enmanuele777@gmail.com",
+	"maintainer": "Enmanuel Ortiz",
+	"website": "https://odooes.com",
+	"description": """Motorcycle Map View""",
+	
+	"depends": ["motorcycle_registry"
+	],
+	"data": [
+		"views/motorcycle_view.xml",
+	],
+	"demo": [
+		
+	],
+	"images": [
+	],
+	"installable": True,
+	"application": False,
+	"auto_install": False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

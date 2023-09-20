33696874f88f66cd8ffecec0dac1b3feca3ee4d8
Enmanuel Ortiz <enmanuele777@gmail.com>
2023-09-19 21:34:52 -0600

    Proyecto Final

--- motorcycle_website/controllers/website.py
+++ motorcycle_website/controllers/website.py
@@ -1,13 +1,11 @@
 #from odoo import models, fields
 
-from odoo import http, _
-
+from odoo import http
 class MotorcycleController(http.Controller):
 	@http.route('/compare', auth='public', website=True)
 	def comparedatas(self, **kw):
 		motores = http.request.env['product.template'].search([('detailed_type', '=', 'motorcycle')])
 		
-		return http.request.render('motorcycle_website.report_compare_motorcicle_product', {
+		return http.request.render('motorcycle_website.report_compare_motorcycle_product', {
 			'motors': motores
 		})
-		
